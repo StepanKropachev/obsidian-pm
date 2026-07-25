018d0e6eccaa39e79e02983e6769ddbb92849662
tara-pogancev <tarapogancev@gmail.com>
2026-07-25 15:44:25 +0200

    Removed unused import

--- src/store/TaskFilter.test.ts
+++ src/store/TaskFilter.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest'
-import { DEFAULT_STATUSES, makeDefaultFilter, makeTask, type CustomFieldFilterSelection, type FilterState, type Task } from '../types'
+import { DEFAULT_STATUSES, makeDefaultFilter, makeTask, type FilterState, type Task } from '../types'
 import {
   applyTaskFilter,
   applyTaskFilterFlat,
